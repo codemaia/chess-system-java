d5aeaa67e31351eb101f2a50c0c22d937b6b10c8
Renan Maia <renanmaia.rl@gmail.com>
2023-03-21 08:11:37 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,15 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello World!");
+		Position pos = new Position(3, 5);
 
+		System.out.println(pos);
 	}
 
 }
